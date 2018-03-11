a13a4426cabd18931f6bc240082c5fb4897d7567
Ubuntu <ubuntu@ip-172-31-40-75.eu-west-3.compute.internal>
2018-03-11 16:31:55 +0000

    S'ha acabat el sistema de registre d'horari, incluit bootstrap i fet les bases del sistema per veure l'horari actual

--- app.js
+++ app.js
@@ -1,54 +1,73 @@
 //MODULS DE NODE
-var express    = require('express');
-var app        = express();
+var express = require('express');
+var app = express();
 var bodyParser = require("body-parser");
 var mongoose = require("mongoose");
-var port     = 8080;
+var port = 8080;
 var moment = require("moment");
 var moment = require('moment-timezone');
 moment().tz("Etc/GMT+2").format();
 //CODES NECESSARIES DIVERSES
 app.set("view engine", "ejs");
 app.use(express.static("public"));
-app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 //CONNEXIÓ DB
 mongoose.connect("mongodb://localhost/dispenser")
-var weekschema = require("./models/weekschema",function(err){
-});
-var pill = require("./models/pill",function(err){});
+var weekschema = require("./models/weekschema", function(err) {});
+var pill = require("./models/pill", function(err) {});
 
- weekschema.remove().exec();
+app.get("/horari", function(req, res) {
 
-app.get("/index", function(req,res){
-//     pill.find({},function(err,pills){
-//             if(err){
-//                     console.log("error");
-//             } else{
-//                     res.render("index");
-//             }
-//     })
-
-
-      res.render("index.ejs");
+  res.render("schedule.ejs");
 
 })
-var schedule = [];
-app.post("/add/schema",function(req,res){
+app.get("/index", function(req, res) {
+  //     pill.find({},function(err,pills){
+  //             if(err){
+  //                     console.log("error");
+  //             } else{
+  //                     res.render("index");
+  //             }
+  //     })
 
-var schema1 = req.body.Monday;
-console.log(schema1);
-weekschema.create({1:schema1},function(err,docs){
-  if(docs){
-    console.log(docs); }
-    else{
+
+  res.render("index.ejs");
+
+})
+
+var schedule = [];
+app.post("/add/schema", function(req, res) {
+  weekschema.remove().exec();
+  var variable;
+  variable = req.body;
+  // console.log(variable);
+  // console.log(schema1);
+  weekschema.create({1:variable[1],2:variable[2]}, function(err, docs) {
+    if (docs) {
+      console.log(docs);
+    } else {
       console.log(err);
     }
-});
-weekschema.find({},function(err,docs){
-  console.log(docs);
-})
+  });
+  // weekschema.findOne({}, function(err, docs) {
+  //   console.log(docs);
+  // })
   res.redirect("../index");
 })
+
+app.get("/prova1",function(req,res){
+
+  weekschema.findOne({}, function(err, docs) {
+    console.log(docs[1].hour);
+    res.render("horari.ejs",{monday:docs});
+
+  })
+
+
+})
+
 // var dia = {
 //   dweek:5
 // };
@@ -67,10 +86,10 @@ weekschema.find({},function(err,docs){
 //         console.log(docs);
 //         console.log("DDAA");
 // });
-app.get("/arduino/removed", function(req,res){
+app.get("/arduino/removed", function(req, res) {
   var time = {
-    hour : moment().hour()+1,
-    minute : moment().minute()
+    hour: moment().hour() + 1,
+    minute: moment().minute()
   };
   console.log(time);
   // pill.create({time});
@@ -80,7 +99,7 @@ app.get("/arduino/removed", function(req,res){
 });
 
 
-app.get("/home", function(req,res){
+app.get("/home", function(req, res) {
   res.render("home.ejs");
 
 });
@@ -115,6 +134,6 @@ app.get("/home", function(req,res){
 
 
 
-app.listen(80, function(){
-    console.log("server is running");
+app.listen(80, function() {
+  console.log("server is running");
 });
--- public/js.js
+++ public/js.js
@@ -1,25 +1,31 @@
 console.log("hedlo");
 var numb = 21;
 var act = 0;
-var schema = {1:{},2:{},3:{},4:{},5:{},6:{},7:{},8:{},9:{},10:{},11:{},12:{},13:{},14:{},
-15:{},16:{},17:{},18:{},19:{},20:{},21:{},
-};
+
 
 var prova1 = {
   hora:1,
   dweek: "Monday"
 };
-schema[1] = prova1;
-console.log(schema);
 $(document).ready(function(){
 
 
     $(".setm").click(function(){
         act++;
-      day = $(this).parent().attr("dia");
+      day = $(this).parent().parent().parent().attr("dia");
       console.log(day);
-      $(this).parent().append("<div class='intro'><div class='field'><labe>Hora</label><input type='text' name='"+day+"[hour]'></div><div class='field'>")
+      console.log(numb);
+          $("#contador").text(numb);
+
+      if(numb!=0){
+      numb--;
+
+      $(this).parent().parent().append("<li class='intro list-group item'><label>Hora</label><input type='text' class='form-control input'name='"+day+"[hour]'></li>")
         // console.log(act);
+  }
+  else {
+    alert("No queden horaris disponibles, has arribat al màxim de 21")
+  }
     });
 
 
